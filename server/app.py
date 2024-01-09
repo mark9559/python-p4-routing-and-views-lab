83bc39273748e3ff56a5ff4ebe0a1ecf14aece6b
mark9559 <mwangimark57@gmail.com>
2024-01-09 15:58:01 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -11,33 +11,32 @@ if __name__ == '__main__':
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string:param>')
-def print_string(param):
-    print(param)
-    return param
+@app.route("/print/<parameter>")
+def print_string(parameter):
+    print(parameter)
+    return parameter
 
-@app.route('/count/<int:param>')
-def count(param):
-    numbers = '<br>'.join(str(num) for num in range(1, param+1))
-    return numbers
 
-@app.route('/math/<float:num1>/<operation>/<float:num2>')
+@app.route("/count/<num>")
+def count(num):
+    count = ""
+    for i in range(int(num)):
+        count += f"{i}" + "\n"
+    return count
+
+
+@app.route("/math/<int:num1>/<operation>/<int:num2>")
 def math(num1, operation, num2):
     result = None
-    if operation == '+':
+    if operation == "+":
         result = num1 + num2
-    elif operation == '-':
+    elif operation == "-":
         result = num1 - num2
-    elif operation == '*':
+    elif operation == "*":
         result = num1 * num2
-    elif operation == 'div':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            return "Error: Division by zero"
-    elif operation == '%':
+    elif operation == "div":
+        result = num1 / num2
+    elif operation == "%":
         result = num1 % num2
-    else:
-        return "Invalid operation"
 
     return str(result)
